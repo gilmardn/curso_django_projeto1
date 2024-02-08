ef286162b27dce311a28df7051e2dfecd9afbca7
gilmardn <gilmar@cotrijuc.com.br>
2024-02-07 22:55:51 -0300

    Comite 07_02_2024

--- receitas/views.py
+++ receitas/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def home(request):
     context = {'name': 'Gilmar Dalla Nora'}
-    return render(request, 'receitas/home.html', context)
+    return render(request, 'receitas/pages/home.html', context)
